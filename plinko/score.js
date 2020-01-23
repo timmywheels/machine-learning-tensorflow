3ff19f71d7acb394335ce5def823d03e0a7c4e39
Tim Wheeler <timothy.wheeler@virginpulse.com>
2020-01-22 20:49:54 -0500

    working on knn algorithm

--- plinko/score.js
+++ plinko/score.js
@@ -1,12 +1,45 @@
 
 const outputs = [];
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
     outputs.push([dropPosition, bounciness, size, bucketLabel]);
-    console.log(outputs);
+    // console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+    const [testSet, trainingSet] = splitDataset(outputs, 10);
+
+    for (let i = 0; i < testSet.length; i++) {
+        const bucket = knn(trainingSet, testSet[i]);
+        console.log(bucket, testSet[i][3]);
+    }
+
 }
 
+function knn(data, point) {
+    return _.chain(data)
+        .map(row => [distance(row[0], point), row[3]])
+        .sortBy(row => row[0])
+        .slice(0, k)
+        .countBy(row => row[1])
+        .toPairs()
+        .sortBy(row => row[1])
+        .last()
+        .first()
+        .parseInt()
+        .value();
+}
+
+function distance(pointA, pointB) {
+    return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+    const shuffled = _.shuffle(data);
+
+    const testSet = _.slice(shuffled, 0, testCount)
+    const trainingSet = _.slice(shuffled, testCount);
+
+    return [testSet, trainingSet];
+}
